326fd9406a3708e7a9058cacb76cf2af0eb3b5fe
Paloma Selva <palomaslva1@gmail.com>
2022-10-19 11:52:32 -0300

    first commit

---
+++ Explorer-lab/src/main.js
@@ -0,0 +1,10 @@
+import './css/style.css';
+
+const bgShadow = window.document.querySelector('.cc svg g:nth-child(3)')
+const bgShadow1 = window.document.querySelector('.cc svg g:nth-child(3) g:nth-child(1) path')
+const bgShadow2 = window.document.querySelector('.cc svg g:nth-child(3) g:nth-child(1) path')
+console.log(bgShadow)
+console.log(bgShadow1)
+console.log(bgShadow2)
+
+//bgShadow1.setAttribute('fill','cyan')
