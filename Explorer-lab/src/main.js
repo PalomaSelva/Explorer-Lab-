5b64cadc066f9d7ae7d7fbfd0eade9a8e2a083ff
Paloma Selva <palomaslva1@gmail.com>
2022-10-19 15:04:02 -0300

    svg

--- Explorer-lab/src/main.js
+++ Explorer-lab/src/main.js
@@ -1,10 +1,25 @@
 import './css/style.css';
 
-const bgShadow = window.document.querySelector('.cc svg g:nth-child(3)')
 const bgShadow1 = window.document.querySelector('.cc svg g:nth-child(3) g:nth-child(1) path')
-const bgShadow2 = window.document.querySelector('.cc svg g:nth-child(3) g:nth-child(1) path')
-console.log(bgShadow)
-console.log(bgShadow1)
-console.log(bgShadow2)
+const bgShadow2 = window.document.querySelector('.cc svg g:nth-child(3) g:nth-child(2) path')
+const Icon = window.document.querySelector('.cc svg path:nth-child(5)')
+console.log(Icon)
 
-//bgShadow1.setAttribute('fill','cyan')
+function setCardType(type){
+    const colors ={
+        visa:["#2D57F2","#436D99"],
+        mastercard:["#DF6F29","#C69347"],
+        default:["black","gray"],
+    }
+
+
+    bgShadow1.setAttribute('fill',colors[type][0])
+    bgShadow2.setAttribute('fill',colors[type][1])
+    
+}
+
+function setIconType(type){
+    
+}
+
+setCardType('default')
