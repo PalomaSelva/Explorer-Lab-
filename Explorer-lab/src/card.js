26a18b880dd631f557a597ecdafb78d06d7b59fe
Paloma Selva <palomaslva1@gmail.com>
2022-10-20 17:18:06 -0300

    usando o iMask

---
+++ Explorer-lab/src/card.js
@@ -0,0 +1,18 @@
+//VISA ^4\d{0,15}
+//inicia com 4, seguido de mais 15 dígitos
+
+
+//MASTER 
+//Inicia com 5, seguido de um dígito entre 1 e 5, seguido de mais 2 dígitos ^5[1-5]\d{0,2}
+// OU
+//Inicia com 22, seguido de  um dígito entre 2 e 9, seguido de mais 1 dígito ^22[2-9]\d
+// OU
+//Inicia com 2, seguido de um dígito entre 3 e 7, seguido de mais 2 dígitos ^2[3-7]\d[0,2] 
+//Seguido de mais 12 dígitos 
+5353535353535353
+
+2251234567890198
+
+2354235646888856
+
+//(2\d[3-7]\d[0,2]|22\d[2-9]\d|5\d[1-5]\d{0,2})\d{0,12}
--- Explorer-lab/src/main.js
+++ Explorer-lab/src/main.js
@@ -1,11 +1,10 @@
 import './css/style.css';
+import IMask from 'imask';
 
 const bgShadow1 = window.document.querySelector('.cc svg > g g:nth-child(1) path')
 const bgShadow2 = window.document.querySelector('.cc svg > g g:nth-child(2) path')
 const ccIcon = window.document.querySelector('.cc-logo span:nth-child(2) img:nth-child(1)')
-console.log(bgShadow1)
-console.log(bgShadow2)
-console.log(ccIcon)
+
 
 function setCardType(type){
     const colors ={
@@ -23,3 +22,58 @@ function setCardType(type){
 
 //setCardType('default')
 globalThis.setCardType = setCardType
+
+//iMask
+
+const cvc = window.document.querySelector('#CVC')
+const cvcPattern  = {mask:'000'}
+const cvcMasked = IMask(cvc, cvcPattern);
+
+const expiration = window.document.querySelector('#expiration')
+const expirationPattern = {
+    mask:'MM{/}YY', 
+    blocks: {
+        YY: {
+           mask: IMask.MaskedRange,
+           from: String(new Date().getFullYear()).slice(2),
+           to:String(new Date().getFullYear()+10).slice(2),
+        },
+
+        MM: {
+            mask: IMask.MaskedRange,
+            from: 1,
+            to: 12
+        },
+    }
+}
+const expirationMasked = IMask(expiration, expirationPattern);
+
+const name = window.document.querySelector('#name');
+const namePattern = {mask:String, min:1,max:10};
+const nameMasked=IMask(name,namePattern);
+
+const cardNumber = window.document.querySelector('#number')
+const cardNumberPattern = {
+    mask:[
+        {
+            mask:'0000 0000 0000 0000',
+            regex:/^4\d{0,15}/,
+            cardType:'visa'
+        },
+        {
+            mask:'0000 0000 0000 0000',
+            regex:/(^2\d[3-7]\d{0,2}|^22\d[2-9]\d|^5\d[1-5]\d{0,2})\d{0,12}/,
+            cardType:'mastercard'
+        },
+        {
+            mask:'0000 0000 0000 0000',
+            cardType:'default'
+        }
+    ],
+    dispatch: function(appended,dynamicMasked){
+        const number = (dynamicMasked.value + appended).replace(/\D\g/,'')
+    }
+}
+console.log(cardNumber)
+
+console.log(window.document.querySelector('#CVC').value)
